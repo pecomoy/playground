7e6661b8cd7aeb157d59cb4a1b87a0003bd033b6
pecomoy <sasa.pejic@pe-com.fi>
2020-09-09 12:17:42 +0300

    Variables

---
+++ home.js
@@ -0,0 +1,10 @@
+//Variables
+
+let foo = "bar";
+console.log("Value of foo is: " + foo);
+
+let number = 42;
+console.log("Value of the number is: " + number);
+
+let age = prompt("How old are you?")
+document.getElementById("someText").innerHTML=age;
